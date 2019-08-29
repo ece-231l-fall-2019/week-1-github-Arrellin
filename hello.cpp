96b2eb1e399c50fc3d94bb5fd272c17aa7c1a0f4
Ariel Arrellin <aarrellin@unm.edu>
2019-08-28 19:03:33 -0600

    second commit.

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,12 @@
 
 int main()
 {
-	std::cout << "# My Heading" << std::endl;
-	std::cout << "# My Heading" << std::endl;
-	std::cout << "# My Heading" << std::endl;
+	std::cout << "# Hello" << std::endl;
+	std::cout << "## Intro" << std::endl;
+	std::cout << "### Hello, my name is ariel. Im from Santa Fe a bit up the highway but originally im from Zacatecas, Mexico. Im a computer engineering major if that wasn't a bit obvious. Im in this class because one 2007 christmas we bought a mac desktop and that was a rabbit hole that lead me here. To me technology is pretty damn near magical, i fell in love with beautiful graphics in games, so i built a pc. I somehow found so much pleasure in trouble shooting my computer and practically everyone elses i knew that yea it just stuck. I love computers and technology but I understand nothing about it. " << std::endl;
+	std::cout << "## Anyways..." << std::endl;
+	
+	std::cout << "# this is my favorite picture on the internet " << std::endl;
+	std::cout << "![cat picture](pancake_cat_1.jpg)" << std::endl;
 }
 
--- hello.md
+++ hello.md
@@ -1,1 +1,6 @@
-# My Heading
+# Hello
+## Intro
+### Hello, my name is ariel. Im from Santa Fe a bit up the highway but originally im from Zacatecas, Mexico. Im a computer engineering major if that wasn't a bit obvious. Im in this class because one 2007 christmas we bought a mac desktop and that was a rabbit hole that lead me here. To me technology is pretty damn near magical, i fell in love with beautiful graphics in games, so i built a pc. I somehow found so much pleasure in trouble shooting my computer and practically everyone elses i knew that yea it just stuck. I love computers and technology but I understand nothing about it. 
+## Anyways...
+# this is my favorite picture on the internet 
+![cat picture](pancake_cat_1.jpg)
